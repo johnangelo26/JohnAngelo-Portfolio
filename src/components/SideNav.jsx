e093e006c7391b6208eeead94a55f9a0328873cc
johnangelo <125664825+johnangelo26@users.noreply.github.com>
2023-07-28 14:39:58 +0800

    initial commit

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -17,7 +17,7 @@ const SideNav = () => {
         <AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden"/>
         {
             nav ? (
-                <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20 bg-gray-700/10'>
+                <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20 bg-gray-900/50'>
                     <a onClick={handleNav}
                         href="#main" 
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
